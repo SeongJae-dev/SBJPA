4378cfcaab08c053872bd46f1a33e28e3ee8e01e
chiron8963@gmail.com <chiron1217@naver.com>
2020-02-05 00:16:16 +0900

    springboot2+jpa+mysql setting

---
+++ berry/src/main/java/com/example/berry/BerryApplication.java
@@ -0,0 +1,13 @@
+package com.example.berry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerryApplication.class, args);
+    }
+
+}
---
+++ berry/src/test/java/com/example/berry/BerryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.berry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@SpringBootTest
+class BerryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
